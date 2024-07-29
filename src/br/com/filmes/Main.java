60d313e0c81822587a22e074ef866a8f2fec4305
Gabriel S. Jesus <gabriel_live12@outlook.com>
2024-07-29 13:23:02 -0300

    Exericios para treinamento de POO em Java

--- src/br/com/filmes/Main.java
+++ src/br/com/filmes/Main.java
@@ -110,5 +110,6 @@ public class Main {
         System.out.println("Tamanho da Lista: " +listaDeFilmes.size());
         System.out.println("Primeiro filme: " +listaDeFilmes.get(0).getNome());
         System.out.println(listaDeFilmes);
+        System.out.println("toString Filme: " + listaDeFilmes.get(0).toString());
     }
 }
